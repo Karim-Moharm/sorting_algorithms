47783c7939844bdccad2ad3b2c31d0647dcef00a
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-24 03:04:24 -0700

    implement another function for insertion list

---
+++ _1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - function used to swap node with its previous node
+ * @list: pointer oints to head of linked list
+ * @node1: A pointer to the first node to be swapped.
+ * @node2:  A pointer to the second node to be swapped.
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (!node1 || !node2 || node1 == node2)
+		return;
+
+	if (node1->prev)
+		node1->prev->next = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+
+	/* address of node1 in node2 and address of node2 in node1 */
+	node1->prev = node2;
+	node2->next = node1;
+
+	if (node1->prev == NULL)
+		*list = node1;
+}
+
+/**
+ * insertion_sort_list - insertion sort using linked list
+ * @list: pointer to the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = NULL;
+	listint_t *tmp = NULL;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	curr = (*list)->next;
+
+	while (curr)
+	{
+		tmp = curr;
+		curr = curr->next;
+
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			swap_nodes(list, tmp->prev, tmp);
+			print_list(*list);
+		}
+	}
+}
