c056e99c22a02e7bc1544f6bb66f821a9d4ce19e
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-24 05:42:59 -0700

    implementing insertion sort using list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,5 +6,24 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	;
+	listint_t *curr = NULL;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	curr = (*list)->next;
+
+	while (curr)
+	{
+		while(curr->prev && curr->n < curr->prev->n)
+		{
+			/* swap operation */
+			curr->prev->next = curr->next;
+			if (curr->next)
+				curr->next->prev = curr->prev;
+			
+
+		}
+	}
+
 }
