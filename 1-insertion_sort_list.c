53c33e3f9fa91f21e44b2ff29cbc82181e493d51
Karim-Moharm <karimmoharm222@gmail.com>
2023-07-24 02:15:24 -0700

    update insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,7 +13,7 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 	if (node1->prev)
 		node1->prev->next = node2;
 	else
-		*list = node2;
+		*list = node1;
 
 	if (node2->next)
 		node2->next->prev = node1;
